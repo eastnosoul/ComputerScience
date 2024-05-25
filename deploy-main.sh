1e59f5a4f1338109bf9a54449309042e0caa1558
eastnosoul <eastnosoul@outlook.com>
2024-05-25 10:10:36 +0800

    new message

--- deploy-main.sh
+++ deploy-main.sh
@@ -1,7 +1,8 @@
-git worktree add ./src/ main
+dir_path = $(pwd)
+git worktree add $dir_path/src/ main
 cd ./src
 git add -A
 git commit -m 'new message'
 git push origin main
-git worktree remove ./src
-cd -
+git worktree remove $dir_path/src
+cd -v
